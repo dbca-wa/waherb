36b4dbb76ac830be57a65ab21d4fda09235a3ce0
Ashley Felton <ashley@ropable.com>
2020-06-22 12:21:18 +0800

    NSL link for ReferenceAdmin.

--- nomenclature/admin.py
+++ nomenclature/admin.py
@@ -27,7 +27,7 @@ class NSLURLFilter(admin.SimpleListFilter):
 @admin.register(Reference)
 class ReferenceAdmin(ModelDescMixin, ActiveAdminMixin, VersionAdmin):
     fields = ('title', 'nsl_url', 'metadata')
-    list_display = ('title_trunc', 'nsl_url', 'modified', 'modifier')
+    list_display = ('title_trunc', 'nsl_url_link', 'modified', 'modifier')
     list_filter = (NSLURLFilter,)
     model_description = """A reference from which taxonomic-relevant information is extracted and
         used to classify specimens into taxonomic groups."""
@@ -37,6 +37,12 @@ class ReferenceAdmin(ModelDescMixin, ActiveAdminMixin, VersionAdmin):
     def title_trunc(self, obj):
         return str(obj)
     title_trunc.short_description = 'title'
+
+    def nsl_url_link(self, obj):
+        if obj.nsl_url:
+            return mark_safe('<a href="{0}" target="_blank">{0}</a>'.format(obj.nsl_url))
+        return ''
+    nsl_url_link.short_description = 'NSL URL'
 
 
 @admin.register(Name)
