3f053633226b3a7c81fbfa0f5a05d8f9a9495f0f
Ashley Felton <ashley@ropable.com>
2020-06-25 12:46:17 +0800

    Added taxon tree link to Name listview, 'has parent' filter.

--- nomenclature/admin.py
+++ nomenclature/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.urls import reverse
 from django.utils.safestring import mark_safe
 from reversion.admin import VersionAdmin
 from waherb.utils import ModelDescMixin, ActiveAdminMixin
@@ -47,10 +48,29 @@ class ReferenceAdmin(ModelDescMixin, ActiveAdminMixin, VersionAdmin):
 
 @admin.register(Name)
 class NameAdmin(ModelDescMixin, VersionAdmin):
+
+    class HasParentFilter(admin.SimpleListFilter):
+        """SimpleListFilter to filter on True/False if an object has a value for parent.
+        """
+        title = 'has parent'
+        parameter_name = 'parent_boolean'
+
+        def lookups(self, request, model_admin):
+            return (
+                ('true', 'True'),
+                ('false', 'False'),
+            )
+
+        def queryset(self, request, queryset):
+            if self.value() == 'true':
+                return queryset.filter(parent__isnull=False)
+            if self.value() == 'false':
+                return queryset.filter(parent__isnull=True)
+
     fields = ('name', 'rank', 'parent', 'basionym', 'references', 'nsl_url', 'metadata')
     filter_horizontal = ('references',)
-    list_display = ('name', 'rank', 'parent', 'nsl_url_link', 'modified', 'modifier')
-    list_filter = ('rank', NSLURLFilter)
+    list_display = ('name', 'rank', 'parent', 'nsl_url_link', 'taxon_tree', 'modified', 'modifier')
+    list_filter = ('rank', NSLURLFilter, HasParentFilter)
     model_description = 'A scientific name that has been classified into a taxonomic tree.'
     raw_id_fields = ('references', 'parent', 'basionym')
     search_fields = ('name', 'rank', 'parent__name', 'basionym__name')
@@ -66,3 +86,6 @@ class NameAdmin(ModelDescMixin, VersionAdmin):
             return mark_safe('<a href="{0}" target="_blank">{0}</a>'.format(obj.nsl_url))
         return ''
     nsl_url_link.short_description = 'NSL URL'
+
+    def taxon_tree(self, obj):
+        return mark_safe('<a href="{}" target="_blank">Link</a>'.format(reverse('nomenclature:taxon_tree', kwargs={'pk': obj.pk})))
